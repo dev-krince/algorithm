fcbe8a49242c9ee3234ff049d4741c0425387d36
kimdonghyun <ssk0080@gmail.com>
2024-08-16 19:21:44 +0900

    바구니 뒤집기

--- src/baekjoon/bronze/바구니_뒤집기.java
+++ src/baekjoon/bronze/바구니_뒤집기.java
@@ -1,7 +1,9 @@
-import java.io.*;
-import java.util.*;
+package baekjoon.bronze;
 
-public class Main {
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class 바구니_뒤집기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
