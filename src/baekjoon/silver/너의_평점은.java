d77dc57f57a9e3fb8486c6ef81304f4886e925b2
kimdonghyun <ssk0080@gmail.com>
2024-08-16 20:01:10 +0900

    너의 평점은

--- src/baekjoon/silver/너의_평점은.java
+++ src/baekjoon/silver/너의_평점은.java
@@ -1,7 +1,9 @@
-import java.io.*;
-import java.util.*;
+package baekjoon.silver;
 
-public class Main {
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class 너의_평점은 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
